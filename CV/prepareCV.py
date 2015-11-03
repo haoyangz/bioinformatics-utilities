7a3b7eacf7c2ef9158ecba544abcaeccd553735a
Haoyang Zeng <zenghaoyang@gmail.com>
2015-11-03 14:11:09 -0500

    use shuffled in prepareCV

--- CV/prepareCV.py
+++ CV/prepareCV.py
@@ -4,7 +4,7 @@
 ### CV: the fold of CV
 ### filedir: The directory of all the files to split
 ### fileprefix: The prefix of all the files to split.
-### filesuffix: The suffix of all the files to split, delimited with '_'. For example, '.txt_.pdf_.ref_.alt'
+### filesuffix: The suffix of all the files to split, delimited with '_'. For example, '.txt_.pdf_.ref_.alt'. All the files fileprefix+filesuffix will be row-shuffled and row-split in the same way.
 ### model: Whether generate valid set. Either 'train_valid_test' or 'train_test'
 ### outputCVnum: The num of cv to output. For example, CV=5, outputCVnum=1 means split the set into 4:1 but output only one of the five possible splits.
 import os,sys
@@ -24,16 +24,16 @@ def file_len(fname):
             pass
         return i + 1
 
-def splitFile(test,train_len,shuffed,data_train,data_valid,data_test):
-    os.system(' '.join(['sed ',str(test[0]+1)+','+str(test[-1]+1)+'d',shuffed,'> temp']))
+def splitFile(test,train_len,shuffled,data_train,data_valid,data_test):
+    os.system(' '.join(['sed ',str(test[0]+1)+','+str(test[-1]+1)+'d',shuffled,'> temp']))
     os.system(' '.join(['head temp -n',str(train_len),'>',data_train]))
     os.system(' '.join(['tail temp -n+'+str(1+train_len),'>',data_valid]))
     os.system('rm temp')
-    os.system(' '.join(['sed -n ',str(test[0]+1)+','+str(test[-1]+1)+'p',shuffed,'>',data_test]))
+    os.system(' '.join(['sed -n ',str(test[0]+1)+','+str(test[-1]+1)+'p',shuffled,'>',data_test]))
 
-def splitFile_novalid(train,test,shuffed,data_train,data_test):
-    os.system(' '.join(['sed ',str(test[0]+1)+','+str(test[-1]+1)+'d',shuffed,'>',data_train]))
-    os.system(' '.join(['sed -n ',str(test[0]+1)+','+str(test[-1]+1)+'p',shuffed,'>',data_test]))
+def splitFile_novalid(train,test,shuffled,data_train,data_test):
+    os.system(' '.join(['sed ',str(test[0]+1)+','+str(test[-1]+1)+'d',shuffled,'>',data_train]))
+    os.system(' '.join(['sed -n ',str(test[0]+1)+','+str(test[-1]+1)+'p',shuffled,'>',data_test]))
 
 if os.path.exists(outdir):
     print('Output folder ' + outdir + ' exists! Will be removed!')
@@ -45,13 +45,13 @@ if filesuffix == 'NA':
 fileprefix_base = os.path.basename(fileprefix)
 suffixes = filesuffix.split('_')
 files = [os.path.join(filedir,fileprefix+x) for x in suffixes]
-files_shuffed = [x+'.shuffed' for x in files]
+files_shuffled = [x+'.shuffled' for x in files]
 
 ### Shuffle the files in the same way
 cmd = ' '.join(['paste -d \'!\' '] + files + [' |  shuf | awk -v FS=\"!\" \'{ '])
-cmd = ' '.join([cmd,'print $1 > ','\"'+files_shuffed[0]+'\"'])
+cmd = ' '.join([cmd,'print $1 > ','\"'+files_shuffled[0]+'\"'])
 for i in range(1,len(files)):
-    cmd = ' '.join([cmd,'; print $'+str(i+1),'>','\"'+files_shuffed[i]+'\"'])
+    cmd = ' '.join([cmd,'; print $'+str(i+1),'>','\"'+files_shuffled[i]+'\"'])
 cmd = ' '.join([cmd,' }\'' ])
 print cmd
 os.system(cmd)
@@ -83,11 +83,11 @@ for train,test in kf:
             for i in xrange(len(test)):
                 f.write(str(test[i]+1)+'\n')
 
-        for t_file in files_shuffed:
+        for t_file in files_shuffled:
             fileprefix_base = os.path.basename(t_file)
     	    splitFile(test,train_len,t_file,os.path.join(cvdir,fileprefix_base+'.train'),os.path.join(cvdir,fileprefix_base+'.valid'),os.path.join(cvdir,fileprefix_base+'.test'))
     else:
-        for t_file in files_shuffed:
+        for t_file in files_shuffled:
             fileprefix_base = os.path.basename(t_file)
             splitFile_novalid(train,test,t_file,os.path.join(cvdir,fileprefix_base+'.train'),os.path.join(cvdir,fileprefix_base+'.test'))
 
