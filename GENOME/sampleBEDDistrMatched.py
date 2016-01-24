685e8097af115a7400850674599ebf56614ae205
Haoyang Zeng <zenghaoyang@gmail.com>
2016-01-24 13:39:29 -0500

    revise sampleBEDDistrMatched.py

--- GENOME/sampleBEDDistrMatched.py
+++ GENOME/sampleBEDDistrMatched.py
@@ -4,10 +4,13 @@ from math import floor
 from os.path import join,exists,realpath,dirname
 bedfile1 =sys.argv[1]
 bedfile2= sys.argv[2]
-column2match = int(sys.argv[3])
-precision = float(sys.argv[4])
+all_column2match = map(int,sys.argv[3].split(','))
+all_column2match = [x-1 for x in all_column2match]
+all_precision = map(float,sys.argv[4].split(','))
 outfile1 = sys.argv[5]
 outfile2 = sys.argv[6]
+column_sep = sys.argv[7]
+totalcol = int(sys.argv[8])
 
 binned1 = tempfile.NamedTemporaryFile().name
 binned2 = tempfile.NamedTemporaryFile().name
@@ -32,7 +35,14 @@ def smooth(bedfile,bin):
     with open(bedfile) as fin, open(bin,'w') as fout:
         for x in fin:
             line = x.strip().split()
-            line[column2match-1] = str(floor(float(line[column2match-1])/precision))
+            for matchidx in range(len(all_precision)):
+                column2match = all_column2match[matchidx]
+                precision  = all_precision[matchidx]
+                line[column2match] = str(floor(float(line[column2match])/precision))
+            t_line = []
+            for idx in all_column2match:
+                t_line.append(line[idx])
+            line.append(column_sep.join(t_line))
             fout.write('%s\n' % '\t'.join(line))
 
 smooth(bedfile1,binned1)
@@ -40,22 +50,21 @@ smooth(bedfile2,binned2)
 
 ### split the file accoding to the smoothed value
 chdir(distr1)
-system(' '.join(['awk \'{print $0 >','$'+str(column2match)+'}\'',binned1]))
+system(' '.join(['awk \'{print $0 >','$'+str(totalcol+1)+'}\'',binned1]))
 chdir(distr2)
-system(' '.join(['awk \'{print $0 >','$'+str(column2match)+'}\'',binned2]))
+system(' '.join(['awk \'{print $0 >','$'+str(totalcol+1)+'}\'',binned2]))
 chdir(cwd)
 
 ### Get the number of sample in each bin
-bins1 = [float(x) for x in next(walk(distr1))[2]]
-bins2 = [float(x) for x in next(walk(distr2))[2]]
+bins1 = [x for x in next(walk(distr1))[2]]
+bins2 = [x for x in next(walk(distr2))[2]]
 
 num1 = {}
 num2 = {}
 for x in bins1:
-    num1[x] = getFileLen(join(distr1,str(x)))
+    num1[x] = getFileLen(join(distr1,x))
 for x in bins2:
-    num2[x] = getFileLen(join(distr2,str(x)))
-
+    num2[x] = getFileLen(join(distr2,x))
 
 ### Match the distribution
 allbin = list(set(bins1 + bins2))
@@ -76,9 +85,9 @@ def resample(topdir,num,outfile):
             continue
         if not flag:
             flag = True
-            system(' '.join(['shuf -n',str(num[x]),join(topdir,str(x)),'>',outfile]))
+            system(' '.join(['shuf -n',str(num[x]),join(topdir,x),'>',outfile]))
         else:
-            system(' '.join(['shuf -n',str(num[x]),join(topdir,str(x)),'>>',outfile]))
+            system(' '.join(['shuf -n',str(num[x]),join(topdir,x),'>>',outfile]))
 
 resample(distr1,num1,outfile1)
 resample(distr2,num2,outfile2)
